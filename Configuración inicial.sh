8d2519d04d5eb9b77fc48cc5da82eb281326a69f
Dario Irribarra <irribarraster@gmail.com>
2020-11-13 14:51:20 -0300

    Configurado nuevo archivo de inicio

--- Configuración inicial.sh
+++ Configuración inicial.sh
@@ -6,6 +6,8 @@ echo "*****Configurando Web Server*****"
 bash ~/TvPost/Bash_files/First_config/WebServer_TvPost.sh
 echo "*****Software de terceros*****"
 bash ~/TvPost/Bash_files/First_config/Third_party_software.sh
+xdotool key ctrl+alt+t
+sleep 3
 echo "Recuerde configurar xscreensaver para deshabilitar el apagado de pantalla"
 sleep 2
 echo "Reiniciando en 5 segundos..."
