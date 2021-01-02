eef8bdbbe064c65af0e63eb940e6a3045618f99d
Dario Irribarra <irribarraster@gmail.com>
2021-01-01 22:03:29 -0300

    Se corrige posición del reloj

--- Bash_files/Apps_interaction/clock.sh
+++ Bash_files/Apps_interaction/clock.sh
@@ -3,8 +3,45 @@
 color_fondo="$1"
 color_letras="$2"
 
-#echo "Color elegido: ${color_fondo}"
 
+width_1=""
+height_1=""
+width_2=""
+height_2=""
+width_3=""
+height_3=""
+
+function widths_heights(){
+	regex='([0-9]):([0-9]+)x([0-9]+)'
+	value=$( cat ~/TvPost/Resolutions/new_resolutions.txt )
+
+	#Get the number of lines to assign variables
+	number_lines=$( wc -l < ~/TvPost/Resolutions/new_resolutions.txt)
+
+
+	while IFS= read -r line; do
+		[[ $line =~ $regex ]]
+
+		if [ ${BASH_REMATCH[1]} == 1 ]; then
+			width_1=${BASH_REMATCH[2]}
+			#height_1=${BASH_REMATCH[3]}
+		fi
+		
+		if [ ${BASH_REMATCH[1]} == 2 ]; then
+			width_2=${BASH_REMATCH[2]}
+			height_2=${BASH_REMATCH[3]}
+		fi
+		
+		#~ if [ ${BASH_REMATCH[1]} == 3 ]; then
+			#~ width_3=${BASH_REMATCH[2]}
+			#~ height_3=${BASH_REMATCH[3]}
+		#~ fi
+		
+	done < <( cat ~/TvPost/Resolutions/new_resolutions.txt )
+	return
+}
+
+widths_heights;
 #runs the app
 python3 /home/pi/TvPost/Py_files/Apps/clock_qt.py ${color_fondo} ${color_letras} &
 
@@ -14,6 +51,7 @@ nueva_id=$( bash ~/TvPost/Bash_files/Apps_interaction/check_new_app_opened.sh )
 #moves the window
 reloj=${nueva_id}
 echo ${reloj}
+xdotool windowmove $reloj $width_1 $height_2;
 
 #Writing the id to later kill it
 echo "4:${reloj}-" >> ~/TvPost/Resolutions/window_id.txt
